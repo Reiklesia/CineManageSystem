268ead22568d3b734835662285ed710860152690
amelieroussin <amelie.roussin@gmail.com>
2025-12-02 18:21:33 -0500

    ajouter des for= et id= dans edit_film, une class dans film.php pour un peu de css, ajouter un (int) devant 2 variables qui prennent des nombres seulement, enlever des espaces inutiles, j'ai ajouté nos noms en commentaire devant les fonctions qu'on a chacun créé dans les models et controleurs et j'ai ajouté de la sécurité à mes fonctions à moi seulement

--- app/views/film.php
+++ app/views/film.php
@@ -1,13 +1,14 @@
 <?php
 require_once __DIR__ . '/../models/FilmModel.php';
 include __DIR__ . '/../includes/header.php';
-
 ?>
 
+<div class="film-details">
 <h2><?php echo htmlspecialchars($film['titre']); ?></h2>
 <p><strong>Réalisateur :</strong> <?php echo htmlspecialchars($film['realisateur']); ?></p>
 <p><strong>Genre :</strong> <?php echo htmlspecialchars($film['genre']); ?></p>
 <p><strong>Année :</strong> <?php echo $film['annee_sortie']; ?></p>
 <p><strong>Description :</strong> <?php echo htmlspecialchars($film['description']); ?></p>
+</div>
 
 <?php include __DIR__ . '/../includes/footer.php'; ?>
