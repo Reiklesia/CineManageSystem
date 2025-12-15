86b8d6b66a7bcd33e1be9f4e0e5d37288ce4f38d
PX264 <beaucheminjeremy@protonmail.com>
2025-12-14 20:25:56 -0500

    add_film

--- app/views/admin/add_film.php
+++ app/views/admin/add_film.php
@@ -1,14 +1,52 @@
 <?php include __DIR__ . '/../../includes/header.php' ?>
 
-<h2>Ajouter un film</h2>
-<form method="POST" action="index.php?action=add_film">
-    <label>Titre: <input type="text" name="titre" required></label><br>
-    <label>Réalisateur: <input type="text" name="realisateur" required></label><br>
-    <label>Genre: <input type="text" name="genre" required></label><br>
-    <label>Année: <input type="number" name="annee_sortie" required></label><br>
-    <label>Description:<br><textarea name="description" required></textarea></label><br>
-    <label>Affiche du film: <input type="file" name="affiche"></label><br>
-    <button type="submit" name="add">Ajouter</button>
-</form>
+<!-- <h2 class="add-film-title">Ajouter un film</h2> -->
+
+<div class="add-film-container">
+    
+    <form method="POST" action="index.php?action=add_film" enctype="multipart/form-data" class="add-film-form">
+
+        <div class="form-left">
+            <label>Titre du film
+                <input type="text" name="titre" required>
+            </label>
+
+            <div class="two-cols">
+                <label>Année de sortie
+                    <input type="date" name="annee_sortie" required>
+                </label>
+
+                <label>Durée (minutes)
+                    <input type="number" name="duree" placeholder="75 minutes" required>
+                </label>
+            </div>
+
+            <label>Genre
+                <select name="genre">
+                    <option value="">Genre...</option>
+                    <option>Action</option>
+                    <option>Drama</option>
+                    <option>Comédie</option>
+                </select>
+            </label>
+
+            <label>Synopsis
+                <textarea name="description" required></textarea>
+            </label>
+        </div>
+
+        <div class="form-right">
+            <div class="image-preview"></div>
+
+            <label class="file-label">Affiche du film
+                <input type="file" name="affiche">
+            </label>
+        </div>
+
+        <button type="submit" name="add" class="btn-submit">Ajouter</button>
+
+    </form>
+</div>
+
 
 <?php include __DIR__ . '/../../includes/footer.php' ?>
