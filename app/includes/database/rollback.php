41c3369747c715ee896a57a92cb2176125adc798
Reiklesia <148357664+Reiklesia@users.noreply.github.com>
2025-12-15 20:59:57 -0500

    Migrate finale

--- app/includes/database/rollback.php
+++ app/includes/database/rollback.php
@@ -2,35 +2,33 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-include __DIR__."/db.php";
+include __DIR__ . "/../db_connect.php";
 
 // recuperer la derniere migration appliquee
-$last = $conn->query("select filename from migrations order by dateApplication desc limit 1")->fetch_assoc();   
-$lastFile = $last['filename'];//001_add_role_to_admins.sql
+$last = $conn->query("select filename from migrations order by applied_at desc limit 1")->fetch_assoc();
+$lastFile = $last['filename'];
 
-$NomfichierRolleback =str_replace(".sql",".rollback.sql",$lastFile);
+$NomfichierRolleback = str_replace(".sql", ".rollback.sql", $lastFile);
 
 
 //chercher le fichier de rollback
-$filepath = __DIR__."/rollbacks/".$NomfichierRolleback;
+$filepath = __DIR__ . "/rollbacks/" . $NomfichierRolleback;
 
-if(!file_exists($filepath))
+if (!file_exists($filepath))
     die("No rollback script for the $$lastFile \n");
-else{
+else {
 
     echo "Rolling back : $lastFile";
-    $sql=file_get_contents($filepath);
+    $sql = file_get_contents($filepath);
 
-    if($conn->multi_query($sql))
-    {
-        while($conn->next_result()){}
+    if ($conn->multi_query($sql)) {
+        while ($conn->next_result()) {
+        }
         echo "\n delete from migrations where filename=' $lastFile' \n";
         $conn->query("delete from migrations where filename= '$lastFile' ");
         echo "Rollback Done!";
-    }
-    else
-    {
-        echo "erreur : ".$conn->error;
+    } else {
+        echo "erreur : " . $conn->error;
     }
 
 
